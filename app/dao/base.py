f2eddbf5cc67ab6bb3dbd112c0499bdbada40b6d
unknown <rrryyy07@gmail.com>
2025-01-24 05:46:53 +0500

    Added users router

--- app/dao/base.py
+++ app/dao/base.py
@@ -17,7 +17,7 @@ class BaseDAO:
     @classmethod
     async def find_one_or_none_by_id(cls, data_id: int):
         async with async_session_maker() as session:
-            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(id=data_id)
+            query = select(cls.model).filter_by(id=data_id)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
-from app.users.auth import get_password_hash
+from fastapi import APIRouter, HTTPException, status, Response, Depends
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserRegister, SUserAuth
+from app.users.dependencies import get_current_user, get_current_admin_user
+from app.users.models import User
 
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
@@ -19,3 +21,26 @@ async def register_user(user_data: SUserRegister) -> dict:
     user_dict['password'] = get_password_hash(user_data.password)
     await UsersDAO.add(**user_dict)
     return {'message': 'Вы успешно зарегистрированы!'}
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: SUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Неверная почта или пароль')
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="users_access_token", value=access_token, httponly=True)
+    return {'access_token': access_token, 'refresh_token': None}
+
+@router.get("/me/")
+async def get_me(user_data: User = Depends(get_current_user)):
+    return user_data
+
+@router.post("/logout/")
+async def logout_user(response: Response):
+    response.delete_cookie(key="users_access_token")
+    return {'message': 'Пользователь успешно вышел из системы'}
+
+@router.get("/all_users/")
+async def get_all_users(user_data: User = Depends(get_current_admin_user)):
+    return await UsersDAO.find_all()
